491bcc8f93806090ece992ce989028e9822e349d
Alok Gudikote <alokomkar.gudikote@gmail.com>
2022-10-18 12:06:00 +0530

    Create Homeactivity.kt

---
+++ HomeActivity.kt
@@ -0,0 +1,36 @@
+package com.alokomkar.activity
+
+import android.app.Activity
+import android.os.Bundle
+
+class HomeActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
